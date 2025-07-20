8e6c4a7b7ca6d765661bc1c40195ac3b44b060fe
navid <navidalvand@gmail.com>
2025-07-20 18:42:00 +0330

    idk

---
+++ android/app/src/main/kotlin/com/example/deepfake/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.deepfake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_webrtc
+import path_provider_foundation
+import video_player_avfoundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+}
